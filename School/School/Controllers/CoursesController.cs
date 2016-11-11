ce80b0515e377b8b7aaeee20409b2bb47506dc07
Ashton Fillery <ashtonfillery@gmail.com>
2016-11-11 08:29:38 -0800

    First commit

---
+++ School/School/Controllers/CoursesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private SchoolEntities db = new SchoolEntities();
+        // GET: Courses
+        public ActionResult Index()
+        {
+            return View(db.Courses.ToList());
+        }
+        //GET: Courses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
