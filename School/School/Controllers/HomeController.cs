ef59d8375632c6cbff53a5c5ded3ed51b52507e4
Ashton Fillery <ashtonfillery@gmail.com>
2016-11-15 15:43:19 -0800

    yes

--- School/School/Controllers/HomeController.cs
+++ School/School/Controllers/HomeController.cs
@@ -8,10 +8,42 @@ namespace School.Controllers
 {
     public class HomeController : Controller
     {
+        SchoolEntities db = new SchoolEntities();
         // GET: Home
         public ActionResult Index()
         {
+            var Info = from detail in db.Students
+                       join stun in db.StudentsCourses on detail.StudentID equals stun.StudentID
+                       join course in db.Courses on stun.CourseID equals course.CourseID
+                       select new
+                       {
+                           AA = detail,
+                           BB = course
+                       };
+
+            return View(Info);
+        }
+        public ActionResult Create()
+        {
+            ViewBag.StudentID = new SelectList(db.Students, "ID", "StudentName");
+            ViewBag.CourseID = new SelectList(db.Courses, "ID", "CourseName");
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="StudentID,CourseID")]StudentsCours Info)
+        {
+            if(ModelState.IsValid)
+            {
+                db.StudentsCourses.Add(Info);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StudentID = new SelectList(db.Students, "ID", "StudentName");
+            ViewBag.CourseID = new SelectList(db.Courses, "ID", "CourseName");
+            return View(Info);
         }
     }
 }
